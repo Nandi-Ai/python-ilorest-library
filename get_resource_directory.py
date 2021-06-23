148198b1e56cacd98d22ae28f091ec7e1624a181
maysbash-cell <83542061+maysbash-cell@users.noreply.github.com>
2021-06-23 13:21:09 +0300

    test bios

---
+++ get_resource_directory.py
@@ -0,0 +1,22 @@
+import sys
+from redfish import RedfishClient
+from redfish.rest.v1 import ServerDownOrUnreachableError
+
+def get_resource_directory(redfishobj):
+
+    try:
+        resource_uri = redfishobj.root.obj.Oem.Hpe.Links.ResourceDirectory['@odata.id']
+    except KeyError:
+        sys.stderr.write("Resource directory is only available on HPE servers.\n")
+        return None
+
+    response = redfishobj.get(resource_uri)
+    resources = []
+
+    if response.status == 200:
+        #sys.stdout.write("\tFound resource directory at /redfish/v1/resourcedirectory" + "\n\n")
+        resources = response.dict["Instances"]
+    else:
+        sys.stderr.write("\tResource directory missing at /redfish/v1/resourcedirectory" + "\n")
+
+    return resources
