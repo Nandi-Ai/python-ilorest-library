7c25b8e561801bafb489b6c2163ac716922dbb7d
Shachar Shapira <shapi78@gmail.com>
2021-06-26 09:30:11 +0300

    BIOS attribute changes

--- radcom/radcom.py
+++ radcom/radcom.py
@@ -6,6 +6,66 @@ from redfish.rest.v1 import ServerDownOrUnreachableError
 from get_resource_directory import get_resource_directory
 
 
+
+def change_bios_setting(_redfishobj, bios_property, property_value):
+
+    bios_uri = None
+    bios_data = None
+    global bios_res
+    resource_instances = get_resource_directory(_redfishobj)
+    if DISABLE_RESOURCE_DIR or not resource_instances:
+        #if we do not have a resource directory or want to force it's non use to find the
+        #relevant URI
+        systems_uri = _redfishobj.root.obj['Systems']['@odata.id']
+        systems_response = _redfishobj.get(systems_uri)
+        systems_members_uri = next(iter(systems_response.obj['Members']))['@odata.id']
+        systems_members_response = _redfishobj.get(systems_members_uri)
+        bios_uri = systems_members_response.obj['Bios']['@odata.id']
+        bios_data = _redfishobj.get(bios_uri)
+
+    else:
+        #Use Resource directory to find the relevant URI
+        for instance in resource_instances:
+            if '#Bios.' in instance['@odata.type']:
+                bios_uri = instance['@odata.id']
+                bios_data = _redfishobj.get(bios_uri)
+                break
+    bios_res = bios_data.obj
+    if bios_data:
+        print("\n\nShowing BIOS attributes before changes:\n\n")
+        print(json.dumps(bios_data.dict, indent=4, sort_keys=True))
+
+    if bios_uri:
+        #BIOS settings URI is needed
+        bios_settings_uri = bios_data.obj['@Redfish.Settings']['SettingsObject']['@odata.id']
+        body = {'Attributes': {bios_property: property_value}}
+        #update BIOS password
+        if bios_property:
+            _redfishobj.property_value = property_value
+        resp = _redfishobj.patch(bios_settings_uri, body)
+
+        #If iLO responds with soemthing outside of 200 or 201 then lets check the iLO extended info
+        #error message to see what went wrong
+        if resp.status == 400:
+            try:
+                print(json.dumps(resp.obj['error']['@Message.ExtendedInfo'], indent=4, \
+                                                                                sort_keys=True))
+            except Exception:
+                sys.stderr.write("A response error occurred, unable to access iLO Extended "\
+                                 "Message Info...")
+        elif resp.status != 200:
+            sys.stderr.write("An http response of \'%s\' was returned.\n" % resp.status)
+        else:
+            print("\nSuccess!\n")
+            print("\n\nShowing BIOS attributes after changes:\n\n")
+            print(json.dumps(resp.dict, indent=4, sort_keys=True))
+            #uncomment if you would like to see the full list of attributes
+            #print("\n\nShowing BIOS attributes after changes:\n\n")
+            #bios_data = _redfishobj.get(bios_uri)
+            #print(json.dumps(bios_data.dict, indent=4, sort_keys=True))
+
+
+
 def get_logicalvolume_actions(volumeIds):
     params = {
         "LogicalDrives": [],
@@ -348,9 +408,10 @@ if __name__ == "__main__":
     # SYSTEM_URL acceptable examples:
     # "https://10.0.0.100"
     # "https://ilo.hostname"
-    SYSTEM_URL = "https://febm-probe3.ilo.ps.radcom.co.il"
-    LOGIN_ACCOUNT = "admin"
-    LOGIN_PASSWORD = "Radmin1234"
+    # SYSTEM_URL = "https://febm-probe3.ilo.ps.radcom.co.il"
+    # LOGIN_ACCOUNT = "admin"
+    # LOGIN_PASSWORD = "Radmin1234"
+
 
     parser = argparse.ArgumentParser(description = "Script to upload and flash NVMe FW")
 
@@ -373,6 +434,29 @@ if __name__ == "__main__":
         sys.stderr.write("ERROR: server not reachable or does not support RedFish.\n")
         sys.exit()
     get_SmartArray_Drives(REDFISHOBJ)
+
+
+    Att_bios = {'ExtendedMemTest': 'Disabled', 'InternalSDCardSlot': 'Disabled','AutoPowerOn': 'PowerOn' \
+                , 'PostF1Prompt': 'Delayed20Sec', 'BootMode': 'LegacyBios', 'FlexLom1Enable': 'Auto', \
+                'RedundantPowerSupply': 'HighEfficiencyAuto', 'PciSlot1Enable': 'HighEfficiencyAuto' \
+                , 'EmbVideoConnection': 'AlwaysEnabled', 'ThermalConfig': 'IncreasedCooling'}
+
+    AttributesElements = Att_bios.items()
+    for ATTRIBUTE, ATTRIBUTE_VAL in AttributesElements:
+        print("--------------------")
+        print(ATTRIBUTE)
+        print("--------------------")
+        change_bios_setting(REDFISHOBJ, ATTRIBUTE, ATTRIBUTE_VAL)
+
+    # Disable PXE to all NICS on board
+    Nics = []
+    for val, att in bios_res['Attributes'].items():
+        if re.match(r'^Slot\dNic*', val):
+            Nics.append(val)
+    for nic in Nics:
+        change_bios_setting(REDFISHOBJ, nic, "Disabled")
+
+
     createLogicalDrive(REDFISHOBJ)
     # get_SmartArray_EncryptionSettings(REDFISHOBJ, DESIRED_PROPERTIES)
 #    delete_SmartArray_LogicalDrives(REDFISHOBJ)
