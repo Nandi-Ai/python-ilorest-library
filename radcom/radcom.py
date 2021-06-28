0a2554b91ff767c500feb1f0fb6b14ebc5a7f547
maysbash-cell <83542061+maysbash-cell@users.noreply.github.com>
2021-06-28 17:58:29 +0300

    update radcom

--- radcom/radcom.py
+++ radcom/radcom.py
@@ -6,6 +6,57 @@ from redfish.rest.v1 import ServerDownOrUnreachableError
 from get_resource_directory import get_resource_directory
 
 
+def mount_virtual_media_iso(_redfishobj, iso_url, media_type, boot_on_next_server_reset):
+
+    virtual_media_uri = None
+    virtual_media_response = []
+
+    resource_instances = get_resource_directory(_redfishobj)
+    if DISABLE_RESOURCE_DIR or not resource_instances:
+        #if we do not have a resource directory or want to force it's non use to find the
+        #relevant URI
+        managers_uri = _redfishobj.root.obj['Managers']['@odata.id']
+        managers_response = _redfishobj.get(managers_uri)
+        managers_members_uri = next(iter(managers_response.obj['Members']))['@odata.id']
+        managers_members_response = _redfishobj.get(managers_members_uri)
+        virtual_media_uri = managers_members_response.obj['VirtualMedia']['@odata.id']
+    else:
+        for instance in resource_instances:
+            #Use Resource directory to find the relevant URI
+            if '#VirtualMediaCollection.' in instance['@odata.type']:
+                virtual_media_uri = instance['@odata.id']
+
+    if virtual_media_uri:
+        virtual_media_response = _redfishobj.get(virtual_media_uri)
+        for virtual_media_slot in virtual_media_response.obj['Members']:
+            data = _redfishobj.get(virtual_media_slot['@odata.id'])
+            if media_type in data.dict['MediaTypes']:
+                virtual_media_mount_uri = data.obj['Actions']['#VirtualMedia.InsertMedia']['target']
+                post_body = {"Image": iso_url}
+
+                if iso_url:
+                    resp = _redfishobj.post(virtual_media_mount_uri, post_body)
+                    if boot_on_next_server_reset is not None:
+                        patch_body = {}
+                        patch_body["Oem"] = {"Hpe": {"BootOnNextServerReset": \
+                                                 boot_on_next_server_reset}}
+                        boot_resp = _redfishobj.patch(data.obj['@odata.id'], patch_body)
+                        if not boot_resp.status == 200:
+                            sys.stderr.write("Failure setting BootOnNextServerReset")
+                    if resp.status == 400:
+                        try:
+                            print(json.dumps(resp.obj['error']['@Message.ExtendedInfo'], indent=4, \
+                                                                                    sort_keys=True))
+                        except Exception as excp:
+                            sys.stderr.write("A response error occurred, unable to access iLO"
+                                             "Extended Message Info...")
+                    elif resp.status != 200:
+                        sys.stderr.write("An http response of \'%s\' was returned.\n" % resp.status)
+                    else:
+                        print("Success!\n")
+                        print(json.dumps(resp.dict, indent=4, sort_keys=True))
+                break
+
 
 def change_bios_setting(_redfishobj, bios_property, property_value):
     #change bios properties in the action settings
@@ -425,16 +476,20 @@ if __name__ == "__main__":
     # LOGIN_ACCOUNT = "admin"
     # LOGIN_PASSWORD = "Radmin1234"
 
+    BOOT_ON_NEXT_SERVER_RESET = True
 
     parser = argparse.ArgumentParser(description = "Script to upload and flash NVMe FW")
 
-    parser.add_argument('-i','--ilo',dest='ilo_address',action="store",help="iLO IP address or FQDN",default="febm-probe3.ilo.ps.radcom.co.il")
+    parser.add_argument('-i','--ilo',dest='ilo_address',action="store",help="iLO IP address or FQDN",default="febm-probe.ilo.ps.radcom.co.il")
     parser.add_argument('-u','--user',dest='ilo_user',action="store",help="iLO username to login",default="admin")
     parser.add_argument('-p','--password',dest='ilo_pass',action="store",help="iLO password to log in.",default="Radmin1234")
     
     args = parser.parse_args()
     system_url = "https://" + args.ilo_address
 
+    MEDIA_URL = "http://172.16.10.226/CentOS-7-x86_64-Minimal-2009-KS-UEFI-GR.iso"
+    #specify the type of content the media represents
+    MEDIA_TYPE = "CD" #current possible options: Floppy, USBStick, CD, DVD
 
     DISABLE_RESOURCE_DIR = False
 
@@ -474,6 +529,8 @@ if __name__ == "__main__":
     # get_SmartArray_EncryptionSettings(REDFISHOBJ, DESIRED_PROPERTIES)
 #    print("")
     reboot_server(REDFISHOBJ)
+    mount_virtual_media_iso(REDFISHOBJ, MEDIA_URL, MEDIA_TYPE, BOOT_ON_NEXT_SERVER_RESET)
+
 
 
     REDFISHOBJ.logout()
